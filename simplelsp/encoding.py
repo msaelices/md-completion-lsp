6f03afdeddd4f9e6768f7f2f064f8e756d6fc6dc
Manuel Saelices <msaelices@gmail.com>
2024-04-14 23:20:25 +0200

    feat: Allow encoding of any serializable obj

--- simplelsp/encoding.py
+++ simplelsp/encoding.py
@@ -1,11 +1,16 @@
 from __future__ import annotations
 
+import json
 from typing import Any
 
 
-def encode_msg(msg: str) -> str:
-    return f'Content-Length: {len(msg)}\r\n\r\n{msg}'
+def encode_msg(msg: Any) -> str:
+    try:
+        serialized = json.dumps(msg)
+    except TypeError:
+        raise ValueError('Message must be JSON serializable')
+    return f'Content-Length: {len(serialized)}\r\n\r\n{serialized}'
 
 def decode_msg(msg: str) -> Any:
-    return msg.split('\r\n\r\n', 1)[1]
+    return json.loads(msg.split('\r\n\r\n', 1)[1])
 
--- tests/test_encoding.py
+++ tests/test_encoding.py
@@ -3,8 +3,8 @@ from simplelsp.encoding import decode_msg, encode_msg
 
 def test_encode_msg():
     expected = 'Content-Length: 17\r\n\r\n{"testing": true}'
-    assert expected == encode_msg('{"testing": true}')
+    assert expected == encode_msg({'testing': True})
 
 def test_decode_msg():
-    expected = '{"testing": true}'
+    expected = {'testing': True}
     assert expected == decode_msg('Content-Length: 17\r\n\r\n{"testing": true}')
