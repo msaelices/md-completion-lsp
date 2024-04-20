b83d21d5d9b970176741fc8821f3d88edfd4107e
Manuel Saelices <msaelices@gmail.com>
2024-04-20 19:46:12 +0200

    test: Test for JsonRpcReader

--- tests/test_jsonrpc.py
+++ tests/test_jsonrpc.py
@@ -1,5 +1,5 @@
 from pytest import raises
-from simplelsp.jsonrpc import decode_msg, encode_msg
+from simplelsp.jsonrpc import decode_msg, encode_msg, JsonRpcReader
 
 
 def test_encode_msg():
@@ -22,3 +22,13 @@ def test_decode_msg():
         ValueError, match='Content-Length does not match actual content length'
     ):
         decode_msg('Content-Length: 17\r\n\r\n{"testing": false}')
+
+
+def test_jsonrpc_reader():
+    messages = []
+    class Consumer:
+        def consume(self, msg):
+            messages.append(msg)
+    reader = JsonRpcReader(consumer=Consumer())
+    reader.feed('Content-Length: 17\r\n\r\n{"testing": true}')
+    assert messages == [{'testing': True}]
