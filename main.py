7fda1eaf45954c0de231711cb8e7929617556809
Manuel Saelices <msaelices@gmail.com>
2024-04-21 08:51:37 +0200

    feat: Move the main script out of the simplelsp package

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
+from __future__ import annotations
+
 import logging
 import sys
 
-from jsonrpc import JsonRpcReader
-from lsp import LspConsumer
+from simplelsp.jsonrpc import JsonRpcReader
+from simplelsp.lsp import LspConsumer
 
 
 logging.basicConfig(
@@ -19,12 +21,9 @@ def main():
     consumer = LspConsumer(stream=sys.stdout, logger=logger)
     reader = JsonRpcReader(consumer=consumer)
     while True:
-        data = sys.stdin.buffer.read(10).decode()
+        data = sys.stdin.buffer.read(1).decode()
         reader.feed(data)
 
 
 if __name__ == '__main__':
-    try:
-        main()
-    except Exception as e:
-        logger.error(f'Error in main: {e}')
+    main()
