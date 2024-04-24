1dbdad0c9f74584811ee05a1841c0adf2bfffc81
Manuel Saelices <msaelices@gmail.com>
2024-04-24 14:13:16 +0200

    feat: Arguments for defining the logging config

--- main.py
+++ main.py
@@ -2,21 +2,26 @@
 
 from __future__ import annotations
 
+import argparse
 import logging
 import sys
 
 from mdcompletion.jsonrpc import JsonRpcReader
 from mdcompletion.lsp import LspConsumer
 
-logging.basicConfig(
-    filename='output.log',
-    level=logging.DEBUG,
-)
-
 logger = logging.getLogger(__name__)
 
 
 def main():
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument('--log-file', help='Log file path', default='mdcompletion.log')
+    arg_parser.add_argument('--log-level', help='Log level', default='DEBUG', choices=('DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'))
+    args = arg_parser.parse_args()
+    
+    logging.basicConfig(
+        filename=args.log_file,
+        level=getattr(logging, args.log_level, logging.INFO),
+    )
     consumer = LspConsumer(stream=sys.stdout, logger=logger)
     reader = JsonRpcReader(consumer=consumer)
     while True:
